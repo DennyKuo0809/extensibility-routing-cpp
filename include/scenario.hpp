5c723d98a382da2936b25f66813e23eff0232bb5
DennyKuo <45343949+DennyKuo0809@users.noreply.github.com>
2023-04-23 14:49:27 +0800

    Add bfs and dfs

--- include/scenario.hpp
+++ include/scenario.hpp
@@ -52,6 +52,15 @@ public:
     /* IO */
     friend std::istream& operator>> (std::istream& is, Scenario& S);
     friend std::ostream& operator<< (std::ostream& os, Scenario& S);
+
+    /* Member Function: About graph */
+    /* BFS to find shortest path in the network */
+    std::vector<int> shortest_path(int src, int dst, int data_rate);
+
+    /* DFS to find all path from src to dst in the network */
+    void dfs(int current, int dst, int dist, int data_rate, int* cur_path, bool* visited, std::vector<std::vector<int> >& path);
+    void all_path(int src, int dst, int data_rate, std::vector<std::vector<int> >& path);
+
 };
 
 #endif
